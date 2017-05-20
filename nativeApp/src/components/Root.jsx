58629b72f74ee5218887597f4f052f902a544306
John Currie <johntcurrie@gmail.com>
2017-05-20 16:52:25 -0700

    Addd props and an increase size function to root

--- nativeApp/src/components/Root.jsx
+++ nativeApp/src/components/Root.jsx
@@ -3,11 +3,25 @@ import { Route, Link } from 'react-router-dom';
 import localStyles from './UtilStyles.css';
 
 class Root extends React.Component {
-  render () {
-    return (
-        <Text>All the tests</Text>
-        {props.list.map( (venue, key) => <ListEntry venueName={venue} key={key}/>)}
-    )
+  constructor(props) {
+    super(props);
+    this.state = {
+      width: 800,
+      height: 500,
+      location: {
+        lat:37.7831708,
+        lng: -122.4100967
+      },
+      show: false
+    }
+  }
+
+  //RELATES TO THE BUTTONS ON THE OUTSIDE OF THE MAP TO MAKE MAP BIGGER OR SMALLER
+  increaseSize() {
+    this.setState({
+      width: this.state.width + 50,
+      height: this.state.height + 50
+    });
   }
 }
 
