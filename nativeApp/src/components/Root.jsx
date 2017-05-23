55647c2adf3fe3357ea2a33e9dbe003fdb850e38
John Currie <johntcurrie@gmail.com>
2017-05-23 11:58:36 -0700

    Added info windows to root component

--- nativeApp/src/components/Root.jsx
+++ nativeApp/src/components/Root.jsx
@@ -6,6 +6,8 @@ class Root extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      width: 21.3453453,
+      height: 173.43535322,
       show: false
     }
   }
@@ -13,9 +15,20 @@ class Root extends React.Component {
   //RELATES TO THE BUTTONS ON THE OUTSIDE OF THE MAP TO MAKE MAP BIGGER OR SMALLER
   increaseSize() {
     this.setState({
-      width: this.state.width + 50,
-      height: this.state.height + 50
+      width: this.state.width + 175,
+      height: this.state.height + 175
     });
+    const infoWindows = this.props.venues.map((venue, x) => {
+            const location = {
+                position: {
+                lat: this.props.venues[x].venue.location.lat,
+                lng: this.props.venues[x].venue.location.lng
+            }
+            }
+            return <InfoWindow key={x} {...location}>
+            {this.props.venues[x].venue.name}
+            </InfoWindow>
+        })
   }
 }
 
