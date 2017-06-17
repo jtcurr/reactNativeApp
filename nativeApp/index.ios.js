29c7c66e54ced585d19afba91f35c5e65dcf888f
John Currie <johntcurrie@gmail.com>
2017-06-16 17:08:21 -0700

    Removed welcome state from android index

--- nativeApp/index.ios.js
+++ nativeApp/index.ios.js
@@ -19,11 +19,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
   instructions: {
     textAlign: 'center',
     color: '#333333',
