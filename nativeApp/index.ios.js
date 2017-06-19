a14fc8268931fe205bce31f51333283e89a8816a
John Currie <johntcurrie@gmail.com>
2017-06-19 14:23:18 -0700

    Removed hardcoded state from android index

--- nativeApp/index.ios.js
+++ nativeApp/index.ios.js
@@ -12,18 +12,6 @@ export default class nativeApp extends Component {
     )
   }
 }
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+
 
 AppRegistry.registerComponent('nativeApp', () => nativeApp);
