ab703d7ca6400799f620cecefe8966f95b1de888
John Currie <johntcurrie@gmail.com>
2017-06-29 22:55:55 -0700

    Added stylesheet to android index

--- nativeApp/index.ios.js
+++ nativeApp/index.ios.js
@@ -12,6 +12,18 @@ export default class nativeApp extends Component {
     )
   }
 }
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  }
+});
 
 AppRegistry.registerComponent('nativeApp', () => nativeApp);
